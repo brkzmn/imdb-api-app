8d29641c3826bcb938022c087489b9d165fb8750
Burak Özmen <brkzmn.wd@gmail.com>
2022-02-17 16:24:44 +0100

    it gets result and grap id of movie

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -6,36 +6,90 @@ import { SEARCH_FIELD_ID } from "../constants.js";
 import { RESULTS_FIELD_ID } from "../constants.js";
 import { RESULTS_LIST_ID } from "../constants.js";
 
+let searchTimeOutToken = 0;
+
 export const initSearchPage = () => {
     const searchElement = getSearchElement();
     const userInterface = document.getElementById(USER_INTERFACE_ID);
     userInterface.appendChild(searchElement);
 
     const searchFieldEl = document.getElementById(SEARCH_FIELD_ID);
-    searchFieldEl.addEventListener("click", searchMovies);
-
+    // searchFieldEl.addEventListener("click", searchMovies);
     showResults();
+    
 }
 
+const showResults = () => {
+    const searchFieldEl = document.getElementById(SEARCH_FIELD_ID);
+/*     searchFieldEl.addEventListener("keyup", (e) => {
+        console.log(e.target.value);
 
-const searchMovies = async () => {
-    const response = await fetch("https://imdb-api.com/en/API/SearchMovie/k_dymzyouh/inception 2010");
-    const jsonData = await response.json();
-    renderResults(jsonData.results);
+        clearTimeout(searchTimeOutToken);
+
+        if(e.target.value.trim().length === 0) {
+            return;
+        }
+
+        searchTimeOutToken = setTimeout(() => {
+            searchMovies(e.target.value)
+            // searchMovies()
+        },250);
+    } ); */
+    searchFieldEl.addEventListener("keyup", (e) => {
+        if(e.target.value.trim().length === 0) {
+            const list = document.getElementById(RESULTS_LIST_ID);
+            list.innerHTML = "please write a movie name";
+            return;
+        }
+        if(e.keyCode === 13) {
+            setTimeout(() => {
+                searchMovies(e.target.value)
+                // searchMovies()
+            },250);
+        }
+    } );
+}
+
+const searchMovies = async (query) => {
+    const searchUrl = `https://www.omdbapi.com/?apikey=fc83c46c&s=${query}`
+    // const searchUrl = `https://imdb-api.com/en/API/Trailer/k_dymzyouh/tt1375666`
+
+    console.log(searchUrl);
+    try {
+        const response = await fetch(searchUrl);
+        const jsonData = await response.json();
+        // console.log(jsonData);
+        renderResults(jsonData.Search);
+        
+    } catch (error) {
+        console.log(error.message);
+        const list = document.getElementById(RESULTS_LIST_ID);
+        list.innerHTML = "movie not found";
+    }
     
 }
 
 const renderResults = (resultsArr) => {
     const list = document.getElementById(RESULTS_LIST_ID);
+    console.log(resultsArr);
+    list.innerHTML = "";
     resultsArr.forEach(movieInfo => {
         const movieItem = document.createElement("li");
-        movieItem.innerText = movieInfo.title;
+        movieItem.innerText = movieInfo.Title;
+        movieItem.id = movieInfo.imdbID;
         list.appendChild(movieItem);
     });
 
+    chooseMovie();
+
 }
 
-const showResults = () => {
-    const searchFieldEl = document.getElementById(SEARCH_FIELD_ID);
-    searchFieldEl.addEventListener("click", searchMovies);
+const chooseMovie = () => {
+    const movieItems = Array.from(document.getElementsByTagName("li"));
+    console.log(movieItems);
+    movieItems.forEach(movieItem => {
+        movieItem.addEventListener("click", (e) => {
+            console.log(e.target.id, "imdb id'si");
+        })
+    })
 }
