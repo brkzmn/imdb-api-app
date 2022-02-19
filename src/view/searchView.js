c6c49fa6ef0bc131f7b3df07784275996406e3c6
Burak Özmen <brkzmn.wd@gmail.com>
2022-02-19 18:09:51 +0100

    modified

--- src/view/searchView.js
+++ src/view/searchView.js
@@ -7,7 +7,7 @@ export const getSearchElement = () => {
     element.classList.add("interface-wrapper")
     element.innerHTML = String.raw`
     <div id="search-wrapper">
-        <label for="site-search">Search movie:</label>
+        <label for="site-search">MovieMANIA</label>
         <input type="text" id="search-field" placeholder="Search movie...">
         <a class='dropdown-trigger btn waves-effect waves-light yellow accent-4' href='#' data-target='dropdown1'>SEARCH <i class="material-icons">search</i></a>
     </div>
